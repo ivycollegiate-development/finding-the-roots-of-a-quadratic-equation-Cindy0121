54e3c0efe77d656d4ca3d15ddd42945b3b490b96
Cindy0121 <cchen25_student@ivycollegiate.org>
2024-10-04 06:06:56 +0000

    add root 1 and root 2

--- Cindy_Sprint_Projectv1.7/Cindy_Sprint_Projectv1.75.py
+++ Cindy_Sprint_Projectv1.7/Cindy_Sprint_Projectv1.75.py
@@ -1,9 +1,10 @@
 import cmath
 
 def calculate_roots(a, b, c):
-    discriminate = b**2 - 4*a*c
+    discriminate = (b**2 - 4*a*c) **0.5
     
-    root_1 = cmath.sqrt(discriminate)
-    root_2 = cmath.sqrt(discriminate)
+    root_1 = (-b+(discriminate)/(2*a))
+    root_2 = (-b-(discriminate)/(2*a))
     return root_1, root_2
-print(calculate_complex_roots(1, 1, 1))
+print(calculate_complex_roots(1, 2, 1))
+print(calculate_complex_roots(1, 5, 6))
