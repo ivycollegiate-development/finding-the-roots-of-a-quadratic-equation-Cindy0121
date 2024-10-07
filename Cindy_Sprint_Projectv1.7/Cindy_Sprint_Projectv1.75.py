cd2dc5e3ec1b47a1fb099496e12c014d9fa0657e
Cindy0121 <cchen25_student@ivycollegiate.org>
2024-10-07 06:16:02 +0000

    add if statement

--- Cindy_Sprint_Projectv1.7/Cindy_Sprint_Projectv1.75.py
+++ Cindy_Sprint_Projectv1.7/Cindy_Sprint_Projectv1.75.py
@@ -1,10 +1,18 @@
 import cmath
 
 def calculate_roots(a, b, c):
-    discriminate = (b**2 - 4*a*c) **0.5
+    discriminate = (b**2 - 4*a*c) 
     
-    root_1 = (-b+(discriminate)/(2*a))
-    root_2 = (-b-(discriminate)/(2*a))
+    root_1 = (-b+cmath.sqrt(discriminate)/(2*a))
+    root_2 = (-b-cmath.sqrt(discriminate)/(2*a))
     return root_1, root_2
+
+if discriminate > 0:
+    print("There are two roots: (root_1) and (root_2)")
+elif discriminate == 0:
+    print("There are one real root at (root_1)")
+else:
+    return root_1, root_2
+
 print(calculate_roots(1, 2, 1))
 print(calculate_roots(1, 5, 6))
