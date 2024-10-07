deac0472bf39aa25965a42a87b8e79d4a3c4ef99
Cindy0121 <cchen25_student@ivycollegiate.org>
2024-10-07 05:42:57 +0000

    Change line 9 and 10

--- Cindy_Sprint_Projectv1.7/Cindy_Sprint_Projectv1.75.py
+++ Cindy_Sprint_Projectv1.7/Cindy_Sprint_Projectv1.75.py
@@ -6,5 +6,5 @@ def calculate_roots(a, b, c):
     root_1 = (-b+(discriminate)/(2*a))
     root_2 = (-b-(discriminate)/(2*a))
     return root_1, root_2
-print(calculate_complex_roots(1, 2, 1))
-print(calculate_complex_roots(1, 5, 6))
+print(calculate_roots(1, 2, 1))
+print(calculate_roots(1, 5, 6))
